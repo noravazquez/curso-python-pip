fc1ef572855e1de5a053585ad8b35369e246666c
Nora Vazquez <102182365+noravazquez@users.noreply.github.com>
2024-05-13 10:31:59 -0600

    Piedra, papel o tijera

---
+++ game/main.py
@@ -0,0 +1,77 @@
+import random
+
+def choose_options():
+  options = ('piedra', 'papel', 'tijera')
+  
+  user_option = input('piedra, papel o tijera => ')
+  user_option = user_option.lower()
+
+  if not user_option in options:
+    print('Esa opción no es válida')
+    # continue
+    return None, None
+
+  computer_option = random.choice(options)
+
+  print(f'User option => {user_option}')
+  print(f'Computer option => {computer_option}')
+
+  return user_option, computer_option
+
+def check_rules(user_option, computer_option, user_wins, computer_wins):
+  if user_option == computer_option:
+    print('Empate!')
+  elif user_option == 'piedra':
+    if computer_option == 'tijera':
+      print('piedra gana a tijera')
+      print('user gano!')
+      user_wins += 1
+    else:
+      print('Papel gana a piedra')
+      print('computer gano!')
+      computer_wins += 1
+  elif user_option == 'papel':
+    if computer_option == 'piedra':
+      print('papel gana a tijera')
+      print('user gano!')
+      user_wins += 1
+    else:
+      print('tijera gana a papel')
+      print('computer gano!')
+      computer_wins += 1
+  elif user_option == 'tijera':
+    if computer_option == 'papel':
+      print('tijera gana a papel')
+      print('user gano!')
+      user_wins += 1
+    else:
+      print('piedra gana a tijera')
+      print('computer gano!')
+      computer_wins += 1
+
+  return user_wins, computer_wins
+
+def run_game():
+  computer_wins = 0
+  user_wins = 0
+  rounds = 1
+  while True:
+    print('Ronda', rounds)
+  
+    print('computer_wins', computer_wins)
+    print('user_wins', user_wins)
+  
+    user_option, computer_option = choose_options()
+  
+    user_wins, computer_wins = check_rules(user_option, computer_option, user_wins, computer_wins)
+  
+    rounds += 1
+  
+    if computer_wins == 2:
+      print('El ganador es la computadora')
+      break
+    if user_wins == 2:
+      print('El ganador es el usuario')
+      break
+
+run_game()
